06ffda1f7947566c85a946b521012b58a0b46a23
Ryan Zhao <rzhao42@icloud.com>
2024-11-02 09:49:22 -0400

    restructured some plugins

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,6 +1,8 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', 'H', '^')
+vim.keymap.set('n', 'L', '$')
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
